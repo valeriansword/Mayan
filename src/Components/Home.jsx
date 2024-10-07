b45bdf859429c2f30e024aeef683bd0393472525
valeriansword <chandrakumarxii@gmail.com>
2024-10-07 20:57:27 +0530

    added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -94,18 +94,18 @@ function Home() {
     </div>
       <div className=' flex flex-col justify-center ml-[10px] space-y-[5px] lg:pl-[20px]'>
         <h1 className='text-3xl font-unbounded font-bold max-sm:text-center bg-clip-text text-transparent bg-gradient-to-b from-[#3a5a40] via-[#3a5a40] to-black bg-[length:100%_100%] bg-gradient-stops-90-10'>Services</h1>
-        <p className='text-lg font-normal text-[#3a5a40]  font-inter'>At MAYAN EXIM GLOBAL TRANSIT, we offer comprehensive logistics and export-import solutions tailored to your needs. From efficient warehousing to air, sea, and ground transportation, our services ensure your goods are delivered safely and on time, whether locally or globally. With our expertise, we streamline your logistics operations, making your business more efficient and cost-effective.
+        <p className='text-lg font-normal text-black  font-inter'>At MAYAN EXIM GLOBAL TRANSIT, we offer comprehensive logistics and export-import solutions tailored to your needs. From efficient warehousing to air, sea, and ground transportation, our services ensure your goods are delivered safely and on time, whether locally or globally. With our expertise, we streamline your logistics operations, making your business more efficient and cost-effective.
         </p>
-        <Link to="/Mayan/Services"><button className='ring-2 font-inter ring-[#3a5a40] max-sm:w-full text-[#dad7cd] bg-gradient-to-b from-[#3a5a40] to-black py-[5px] w-[250px] px-2 rounded-md'>Know More</button></Link>
+        <Link to="/Mayan/Services" className='flex justify-center'><button className='ring-2 font-inter ring-[#3a5a40] max-sm:w-full text-[#dad7cd] bg-gradient-to-b from-[#3a5a40] to-black py-[5px] w-[250px] px-2 rounded-md'>Know More</button></Link>
       </div>
       
     </div>
     {/* products */}
     <div className='w-full flex max-sm:flex-col-reverse justify-around px-[20px]'>
-    <div className=' flex flex-col justify-center ml-[10px] space-y-[5px] lg:pl-[20px]'>
+    <div className=' flex flex-col justify-center ml-[10px] space-y-[5px] lg:pr-[20px]'>
         <h1 className='text-3xl  bg-clip-text font-unbounded text-transparent bg-gradient-to-b from-[#3a5a40] via-[#3a5a40] to-black bg-[length:100%_100%] bg-gradient-stops-90-10 font-bold max-sm:text-center'>Products</h1>
-        <p className='text-lg font-normal  text-[#3a5a40] font-inter '>We provide a diverse range of premium products, carefully sourced and delivered with excellence. From fresh agricultural goods to essential commodities, we guarantee quality and consistency. Our product offerings are tailored to meet the demands of businesses across industries, ensuring reliability in every order.</p>
-        <Link to="/Mayan/Products/"><button className='ring-2 font-inter ring-[#3a5a40] max-sm:w-full text-[#dad7cd] bg-gradient-to-b from-[#3a5a40] to-black py-[5px] w-[250px] px-2 rounded-md'>Know More</button></Link>
+        <p className='text-lg font-normal  text-black font-inter '>We provide a diverse range of premium products, carefully sourced and delivered with excellence. From fresh agricultural goods to essential commodities, we guarantee quality and consistency. Our product offerings are tailored to meet the demands of businesses across industries, ensuring reliability in every order.</p>
+        <Link to="/Mayan/Products/" className='flex justify-center'><button className='ring-2 font-inter ring-[#3a5a40] max-sm:w-full text-[#dad7cd] bg-gradient-to-b from-[#3a5a40] to-black py-[5px] w-[250px] px-2 rounded-md'>Know More</button></Link>
       </div>
       
     <div className="w-1/2 max-w-4xl xl:max-w-2xl 2xl:max-w-3xl py-8 max-sm:w-full ">
