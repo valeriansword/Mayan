8b4fc8529c71f941367dab2a818867f24e2cc6ce
valeriansword <chandrakumarxii@gmail.com>
2024-10-09 20:51:25 +0530

    added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import vebzart from "../assets/vebzart.png";
 const Footer = () => {
   return (
-    <div className="text-[#3a5a40] bg-gradient-to-b from-[#dad7cd] to-[#ffffff] text-lg font-semibold py-[80px] max-sm:py-[50px]">
+    <div>
+    <div className="text-[#3a5a40] bg-gradient-to-b from-[#dad7cd] to-[#ffffff] text-lg font-semibold lg:py-[80px]  max-sm:py-[50px]">
         <footer className=" flex justify-around ">
             <div>
                 <Link to="/Mayan"><h1>Home</h1></Link>
@@ -15,7 +16,7 @@ const Footer = () => {
                 
             </div>
             <div>
-                <Link to="/Mayan/Certificate"><h1>certificate</h1></Link>
+                <Link to="/Mayan/Certificate"><h1>Certificate</h1></Link>
                 <Link to="/Mayan/Services"><h1>Services</h1></Link>
                 <Link to="/Mayan/Products"><h1>Products</h1></Link>
             </div>
@@ -24,11 +25,14 @@ const Footer = () => {
                 <a href="https://www.linkedin.com/company/mayan-exim-global-transit/" target="new"><h1>Linked In</h1></a>                
                 <a href="https://wa.me/918098787661" target="_blank" rel="noopener noreferrer"><h1>Whatsapp</h1></a>
                 <a href="https://mailto:mayanexim@gmail.com"><h1>E-mail</h1></a>
-                <span><img src={vebzart} className="h-[60px] w-[80px] mt-[20px]"/></span>
+                <span><img src={vebzart} className="h-[60px] w-[80px] mt-[20px] max-sm:hidden"/></span>
             </div>
             
         </footer>
             <h1 className=" pt-[30px] text-center md:hidden">mayan exim global transit ®</h1>
+            
+        </div>
+        <span className="flex justify-center"><img src={vebzart} className="h-[60px] w-[80px] "/></span>
         </div>
       );
 };
